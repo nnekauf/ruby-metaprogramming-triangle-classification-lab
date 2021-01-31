40bbadf7d61adb342c8c0a829afd6069d3279312
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-01-31 00:31:28 -0500

    commit tree 9438be2862c4c6568d989223695fd27139b4fb6f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
 class Triangle
-  # write code here
+
+  attr_accessor :side1, :side2, :side3
+
+  @@all = []
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    @@all << @side1
+    @@all << @side2
+    @@all << @side3
+    #@@all.sort
+  end
+
+  def kind
+      if invalid_triangle
+      raise TriangleError
+      elsif @side1 == @side2 && @side1 == @side3
+        :equilateral
+      elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
+        :isosceles
+      elsif @side1 != @side2 && @side1 != @side3 && @side2 != @side3
+        :scalene
+      end
+  end
+  def invalid_triangle
+      if @@all.any? {|side| side == 0} #@side1 == 0 || @side2 == 0 || @side3 == 0  #
+        raise TriangleError
+      elsif @side1 + @side2 <= @side3 || @side1 + @side3 <= @side2 || @side3 + @side2 <= @side1
+        raise TriangleError
+      end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "no"
+    end
+  end
+
 end
